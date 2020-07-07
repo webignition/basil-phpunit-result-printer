dea66455333bad5206c6c7d1ce7b6c6faea6051c
Jon Cram <webignition@gmail.com>
2020-07-07 19:10:48 +0100

    Create SourceFactory (#77)

---
+++ src/Factory/Model/Source/SourceFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter\Factory\Model\Source;
+
+use webignition\BasilIdentifierAnalyser\IdentifierTypeAnalyser;
+use webignition\BasilPhpUnitResultPrinter\FooModel\Source\SourceInterface;
+
+class SourceFactory
+{
+    private NodeSourceFactory $nodeSourceFactory;
+    private ScalarSourceFactory $scalarSourceFactory;
+    private IdentifierTypeAnalyser $identifierTypeAnalyser;
+
+    public function __construct(
+        NodeSourceFactory $nodeSourceFactory,
+        ScalarSourceFactory $scalarSourceFactory,
+        IdentifierTypeAnalyser $identifierTypeAnalyser
+    ) {
+        $this->nodeSourceFactory = $nodeSourceFactory;
+        $this->scalarSourceFactory = $scalarSourceFactory;
+        $this->identifierTypeAnalyser = $identifierTypeAnalyser;
+    }
+
+    public static function createFactory(): self
+    {
+        return new SourceFactory(
+            NodeSourceFactory::createFactory(),
+            ScalarSourceFactory::createFactory(),
+            IdentifierTypeAnalyser::create()
+        );
+    }
+
+    public function create(string $source): ?SourceInterface
+    {
+        if ($this->identifierTypeAnalyser->isDomOrDescendantDomIdentifier($source)) {
+            return $this->nodeSourceFactory->create($source);
+        }
+
+        return $this->scalarSourceFactory->create($source);
+    }
+}
