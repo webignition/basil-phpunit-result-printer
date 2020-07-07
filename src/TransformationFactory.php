04d945fc7aabe24c56e8e6db8f2ff83fdafa95b1
Jon Cram <webignition@gmail.com>
2020-07-07 12:01:51 +0100

    Create TransformationFactory (#64)

---
+++ src/TransformationFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter;
+
+use webignition\BasilModels\Action\ResolvedAction;
+use webignition\BasilModels\Assertion\DerivedValueOperationAssertion;
+use webignition\BasilModels\Assertion\ResolvedAssertion;
+use webignition\BasilModels\EncapsulatingStatementInterface;
+use webignition\BasilModels\StatementInterface;
+use webignition\BasilPhpUnitResultPrinter\FooModel\Statement\Transformation;
+
+class TransformationFactory
+{
+    /**
+     * @param StatementInterface $statement
+     *
+     * @return Transformation[]
+     */
+    public function createTransformations(StatementInterface $statement): array
+    {
+        $transformations = [];
+
+        while ($statement instanceof EncapsulatingStatementInterface) {
+            $sourceStatement = $statement->getSourceStatement();
+            $sourceStatementSource = $sourceStatement->getSource();
+
+            if ($statement instanceof DerivedValueOperationAssertion) {
+                $transformations[] = new Transformation(Transformation::TYPE_DERIVATION, $sourceStatementSource);
+            }
+
+            if ($statement instanceof ResolvedAction || $statement instanceof ResolvedAssertion) {
+                $transformations[] = new Transformation(Transformation::TYPE_RESOLUTION, $sourceStatementSource);
+            }
+
+            $statement = $sourceStatement;
+        }
+
+        return $transformations;
+    }
+}
