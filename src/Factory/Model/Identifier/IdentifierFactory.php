9bdaf16a597894ec625ec5ff281009449a890453
Jon Cram <webignition@gmail.com>
2020-07-07 15:14:17 +0100

    Create Identifier from string

---
+++ src/Factory/Model/Identifier/IdentifierFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter\Factory\Model\Identifier;
+
+use webignition\BasilPhpUnitResultPrinter\FooModel\Identifier\Identifier;
+use webignition\BasilPhpUnitResultPrinter\FooModel\Identifier\Properties;
+
+class IdentifierFactory
+{
+    private PropertiesFactory $propertiesFactory;
+
+    public function __construct(PropertiesFactory $propertiesFactory)
+    {
+        $this->propertiesFactory = $propertiesFactory;
+    }
+
+    public static function createFactory(): self
+    {
+        return new IdentifierFactory(
+            PropertiesFactory::createFactory()
+        );
+    }
+
+    public function create(string $source): ?Identifier
+    {
+        $properties = $this->propertiesFactory->create($source);
+
+        if ($properties instanceof Properties) {
+            return new Identifier($source, $properties);
+        }
+
+        return null;
+    }
+}
