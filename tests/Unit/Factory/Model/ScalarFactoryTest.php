4329e0ecf854b855c1434bc5a42ba187f402f15c
Jon Cram <webignition@gmail.com>
2020-07-07 18:56:50 +0100

    Create ScalarSourceFactory (#76)

---
+++ tests/Unit/Factory/Model/ScalarFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter\Tests\Unit\Model\Factory;
+
+use webignition\BasilPhpUnitResultPrinter\Factory\Model\ScalarFactory;
+use webignition\BasilPhpUnitResultPrinter\FooModel\Scalar;
+use webignition\BasilPhpUnitResultPrinter\Tests\Unit\AbstractBaseTest;
+
+class ScalarFactoryTest extends AbstractBaseTest
+{
+    private ScalarFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = ScalarFactory::createFactory();
+    }
+
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(string $source, ?Scalar $expectedScalar)
+    {
+        self::assertEquals($expectedScalar, $this->factory->create($source));
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'source' => '',
+                'expectedScalar' => null,
+            ],
+            'browser property: invalid' => [
+                'source' => '$browser.invalid',
+                'expectedScalar' => null,
+            ],
+            'browser property: valid' => [
+                'source' => '$browser.size',
+                'expectedScalar' => new Scalar(
+                    Scalar::TYPE_BROWSER_PROPERTY,
+                    '$browser.size'
+                ),
+            ],
+            'data parameter: invalid' => [
+                'source' => '$datakey',
+                'expectedScalar' => null,
+            ],
+            'data parameter: valid' => [
+                'source' => '$data.key',
+                'expectedScalar' => new Scalar(
+                    Scalar::TYPE_DATA_PARAMETER,
+                    '$data.key'
+                ),
+            ],
+            'environment parameter: invalid' => [
+                'source' => '$envkey',
+                'expectedScalar' => null,
+            ],
+            'environment parameter: valid' => [
+                'source' => '$env.key',
+                'expectedScalar' => new Scalar(
+                    Scalar::TYPE_ENVIRONMENT_PARAMETER,
+                    '$env.key'
+                ),
+            ],
+            'literal: invalid' => [
+                'source' => 'literal',
+                'expectedScalar' => null,
+            ],
+            'literal: valid' => [
+                'source' => '"literal"',
+                'expectedScalar' => new Scalar(
+                    Scalar::TYPE_LITERAL,
+                    '"literal"'
+                ),
+            ],
+            'page property: invalid' => [
+                'source' => '$pageproperty',
+                'expectedScalar' => null,
+            ],
+            'page property: valid' => [
+                'source' => '$page.url',
+                'expectedScalar' => new Scalar(
+                    Scalar::TYPE_PAGE_PROPERTY,
+                    '$page.url'
+                ),
+            ],
+        ];
+    }
+}
