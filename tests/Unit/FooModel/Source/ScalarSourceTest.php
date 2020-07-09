70dcad2a05ee0b7b06b92665425b5e0c9ec4e21b
Jon Cram <webignition@gmail.com>
2020-07-09 14:45:52 +0100

    Update source tests

--- tests/Unit/FooModel/Source/ScalarSourceTest.php
+++ tests/Unit/FooModel/Source/ScalarSourceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace webignition\BasilPhpUnitResultPrinter\Tests\Unit\FooModel\Source;
 
+use webignition\BasilPhpUnitResultPrinter\Factory\Model\ScalarFactory;
 use webignition\BasilPhpUnitResultPrinter\FooModel\Scalar;
 use webignition\BasilPhpUnitResultPrinter\FooModel\Source\ScalarSource;
 use webignition\BasilPhpUnitResultPrinter\Tests\Unit\AbstractBaseTest;
@@ -25,10 +26,7 @@ class ScalarSourceTest extends AbstractBaseTest
     {
         return [
             'scalar' => [
-                'body' => new Scalar(
-                    Scalar::TYPE_LITERAL,
-                    'literal'
-                ),
+                'body' => \Mockery::mock(Scalar::class),
             ],
         ];
     }
@@ -46,20 +44,16 @@ class ScalarSourceTest extends AbstractBaseTest
 
     public function getDataDataProvider(): array
     {
+        $scalarFactory = ScalarFactory::createFactory();
+
+        $scalar = $scalarFactory->create('"literal"') ?? \Mockery::mock(Scalar::class);
+
         return [
             'scalar' => [
-                'source' => new ScalarSource(
-                    new Scalar(
-                        Scalar::TYPE_LITERAL,
-                        'literal'
-                    )
-                ),
+                'source' => new ScalarSource($scalar),
                 'expectedData' => [
                     'type' => 'scalar',
-                    'body' => [
-                        'type' => Scalar::TYPE_LITERAL,
-                        'value' => 'literal',
-                    ],
+                    'body' => $scalar->getData(),
                 ],
             ],
         ];
