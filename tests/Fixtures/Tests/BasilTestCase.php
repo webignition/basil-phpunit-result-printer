260696b6ce97779f1f5f51e3dff237fe16424085
Jon Cram <webignition@gmail.com>
2020-08-27 17:18:17 +0100

    Populate ResultPrinter::addError()

---
+++ tests/Fixtures/Tests/BasilTestCase.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter\Tests\Fixtures\Tests;
+
+use PHPUnit\Framework\TestCase;
+use webignition\BaseBasilTestCase\BasilTestCaseInterface;
+use webignition\BasilModels\DataSet\DataSetInterface;
+use webignition\DomElementIdentifier\ElementIdentifierInterface;
+
+class BasilTestCase extends TestCase implements BasilTestCaseInterface
+{
+    public static function setBasilTestPath(string $testPath): void
+    {
+    }
+
+    public static function getBasilTestPath(): string
+    {
+        return '';
+    }
+
+    public function setBasilStepName(string $stepName): void
+    {
+    }
+
+    public function getBasilStepName(): string
+    {
+        return '';
+    }
+
+    public function getHandledStatements(): array
+    {
+        return [];
+    }
+
+    public function setExaminedValue(?string $examinedValue): void
+    {
+    }
+
+    public function setExpectedValue(?string $expectedValue): void
+    {
+    }
+
+    public function getExaminedValue(): ?string
+    {
+        return null;
+    }
+
+    public function getExpectedValue(): ?string
+    {
+        return null;
+    }
+
+    public function setBooleanExaminedValue(bool $examinedValue): void
+    {
+    }
+
+    public function setBooleanExpectedValue(bool $expectedValue): void
+    {
+    }
+
+    public function getBooleanExaminedValue(): ?bool
+    {
+        return null;
+    }
+
+    public function getBooleanExpectedValue(): ?bool
+    {
+        return null;
+    }
+
+    public function getExaminedElementIdentifier(): ?ElementIdentifierInterface
+    {
+        return null;
+    }
+
+    public function getExpectedElementIdentifier(): ?ElementIdentifierInterface
+    {
+        return null;
+    }
+
+    public function getLastException(): ?\Throwable
+    {
+        return null;
+    }
+
+    public function setCurrentDataSet(?DataSetInterface $dataSet): void
+    {
+    }
+
+    public function getCurrentDataSet(): ?DataSetInterface
+    {
+        return null;
+    }
+}
