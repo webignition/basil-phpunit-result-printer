9f70e1da1bd1391f0a7734d6428f307d4408325a
Jon Cram <webignition@gmail.com>
2025-12-09 11:32:36 +0000

    Fix IdentifierTest

--- tests/Unit/Model/Identifier/IdentifierTest.php
+++ tests/Unit/Model/Identifier/IdentifierTest.php
@@ -68,9 +68,9 @@ class IdentifierTest extends AbstractBaseTestCase
     /**
      * @dataProvider isAttributeDataProvider
      */
-    public function testIsAttribute(Identifier $identifier, bool $expectedIsAttribute): void
+    public function testIsAttribute(Identifier $identifier, bool $expected): void
     {
-        self::assertSame($expectedIsAttribute, $identifier->isAttribute());
+        self::assertSame($expected, $identifier->isAttribute());
     }
 
     /**
@@ -93,11 +93,11 @@ class IdentifierTest extends AbstractBaseTestCase
         return [
             'not attribute' => [
                 'identifier' => new Identifier('$".selector"', $elementProperties),
-                'expectedHasAttribute' => false,
+                'expected' => false,
             ],
             'is attribute' => [
                 'identifier' => new Identifier('$".selector".attribute_name', $attributeProperties),
-                'expectedHasAttribute' => true,
+                'expected' => true,
             ],
         ];
     }
