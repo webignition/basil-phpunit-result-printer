77017062346714234229642c43194b49eea865ae
Jon Cram <webignition@gmail.com>
2025-12-10 16:41:54 +0000

    Intialise printer within extension

--- src/ResultPrinterExtension.php
+++ src/ResultPrinterExtension.php
@@ -8,15 +8,24 @@ use PHPUnit\Runner\Extension\Extension;
 use PHPUnit\Runner\Extension\Facade;
 use PHPUnit\Runner\Extension\ParameterCollection;
 use PHPUnit\TextUI\Configuration\Configuration;
+use PHPUnit\TextUI\Output\Printer;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\BeforeFirstTestMethodErroredSubscriber;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\ErroredSubscriber;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\FailedSubscriber;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\FinishedSubscriber;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\PassedSubscriber;
 use webignition\BasilPhpUnitResultPrinter\Subscriber\Test\PreparedSubscriber;
+use PHPUnit\TextUI\Output\DefaultPrinter;
 
-class ResultPrinterExtension implements Extension
+readonly class ResultPrinterExtension implements Extension
 {
+    private Printer $printer;
+
+    public function __construct()
+    {
+        $this->printer =DefaultPrinter::standardOutput();
+    }
+
     public function bootstrap(Configuration $configuration, Facade $facade, ParameterCollection $parameters): void
     {
         if ($configuration->noOutput()) {
