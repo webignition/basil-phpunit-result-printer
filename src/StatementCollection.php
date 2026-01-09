f6e64a24f21fd50c3135654abff46580979db2e3
Jon Cram <webignition@gmail.com>
2026-01-09 10:09:03 +0000

    foo

---
+++ src/StatementCollection.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilPhpUnitResultPrinter;
+
+use webignition\BasilModels\Model\StatementInterface;
+
+class StatementCollection
+{
+    private ?StatementInterface $failedStatement = null;
+
+    /**
+     * @param StatementInterface[] $statements
+     */
+    public function __construct(
+        private readonly array $statements,
+    ) {}
+
+    public function setFailedStatement(StatementInterface $statement): void
+    {
+        $this->failedStatement = $statement;
+    }
+
+    /**
+     * @return StatementInterface[]
+     */
+    public function getHandledStatements(): array
+    {
+        if (null === $this->failedStatement) {
+            return $this->statements;
+        }
+
+        $handledStatements = [];
+        $hasFoundFailedStatement = false;
+
+        foreach ($this->statements as $statement) {
+            if ($statement->getIndex() === $statement->getIndex()) {
+                $hasFoundFailedStatement = true;
+            }
+
+            if (!$hasFoundFailedStatement) {
+                $handledStatements[] = $statement;
+            }
+        }
+
+        return $handledStatements;
+    }
+}
