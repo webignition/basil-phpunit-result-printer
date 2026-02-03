3c3539f4235812872d4d7ee84f3117e511be61a9
Jon Cram <webignition@gmail.com>
2026-02-03 16:37:59 +0000

    Use base-basil-test-case ^17 (#387)

--- tests/Fixtures/Tests/BasilTestCase.php
+++ tests/Fixtures/Tests/BasilTestCase.php
@@ -6,8 +6,16 @@ namespace webignition\BasilPhpUnitResultPrinter\Tests\Fixtures\Tests;
 
 use PHPUnit\Framework\TestCase;
 use webignition\BaseBasilTestCase\ClientManager;
+use webignition\BaseBasilTestCase\Message\Factory;
 
 class BasilTestCase extends TestCase
 {
+    protected static Factory $messageFactory;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$messageFactory = Factory::createFactory();
+    }
+
     public static function setClientManager(ClientManager $clientManager): void {}
 }
